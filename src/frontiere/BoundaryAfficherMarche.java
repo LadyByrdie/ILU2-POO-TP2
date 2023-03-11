f120b63d86de5017814a7b60de603e7e43acfed5
LadyByrdie <eladden502@gmail.com>
2023-03-11 01:44:57 +0100

    7 finit dfdsf xx

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -14,12 +14,17 @@ public class BoundaryAfficherMarche {
 		infosMarche=controlAfficherMarche.donnerInfosMarche();
 		
 		if(infosMarche.length==0) {
-			System.out.println("Le marché est vide, revenes plus tard.");
+			System.out.println("Le marchï¿½ est vide, revenes plus tard.");
 		}else {
-			System.out.println(nomAcheteur+ " ,vous trouverez au marché.");
-			for(int i=0;i<infosMarche.length;i++) {
-				
-				System.out.println("- "+vendeur+" qui vend "+ quantite+" "+produit);
+			System.out.println(nomAcheteur+ " ,vous trouverez au marchï¿½.");
+			for(int i=0;i<infosMarche.length;i++){
+				if((i%3==0)&&(i>2||i==0)) {
+					String vendeur=infosMarche[i];
+					String quantite=infosMarche[i+1];
+					String produit=infosMarche[i+2];
+					System.out.println("- "+vendeur+" qui vend "+ quantite+" "+produit);
+
+				}
 			}
 		}
 	}
